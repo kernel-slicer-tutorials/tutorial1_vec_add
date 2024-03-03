bcd66371def2b98c5294a14b124d8e45e5c82819
AlexandrShcherbakov <alex.shcherbakov.as@gmail.com>
2024-03-03 18:18:20 +0300

    Make run_kslicer more customizable
    Now user should run "setup.sh path/to/kslicer.exe
    path/to/kslicer/folder" first.
    This script generates config.txt that is used by run_kslicer.sh.

--- run_kslicer.sh
+++ run_kslicer.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-kslicer_directory="/home/frol/PROG/kernel_slicer"
+if [ ! -f config.txt ]; then
+  echo "File config.txt not found! Run 'setup.sh path/to/kslicer.exe path/to/kslicer/folder' first!"
+  exit 1
+fi
+unset -v kslicer_exe kslicer_directory
+for var in kslicer_exe kslicer_directory; do
+  IFS= read -r "$var" || break
+done < config.txt
 current_directory=$(pwd)           # получаем текущую директорию и сохраняем в переменную
 cd "$kslicer_directory" || exit 1  # переходим в директорию слайсера, запускать нужно из неё
 
-$kslicer_directory/cmake-build-release/kslicer $current_directory/test.cpp \
+$kslicer_exe $current_directory/test.cpp \
 -mainClass "Test" \
 -pattern "ipv" \
 -shaderCC "glsl" \
@@ -11,4 +18,4 @@ $kslicer_directory/cmake-build-release/kslicer $current_directory/test.cpp \
 -stdlibfolder "$kslicer_directory/TINYSTL" \
 -I$kslicer_directory/TINYSTL "ignore" \
 -DKERNEL_SLICER \
--v 
+-v
